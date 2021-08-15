036c3c2e179423b260da27cc955ffdc9028254f8
ac <ac@ziposoft.com>
2021-08-15 15:55:33 -0400

    menu mostly working. needs some touch up.

--- public/mod/MenuClient.ts
+++ public/mod/MenuClient.ts
@@ -6,7 +6,7 @@ import {PageClientMenu, PageClientMenuT} from "./ClientPage.js"
 import {IInput} from "./IMenu.js";
 console.log("MenuClient module");
 
-const DEBUG=true;
+const DEBUG=false;
 function DBG(...args: any[]) { if(DEBUG) console.log(...args);};
 
 
